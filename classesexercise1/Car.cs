ac70d8506796e32ef7f126a72ae3e48cba525a9b
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-01-20 12:25:51 -0500

    finished

---
+++ classesexercise1/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classesexercise1
+{
+    public class Car
+    {
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car()
+        {   
+        }
+        public string Make { get; set; }    
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
